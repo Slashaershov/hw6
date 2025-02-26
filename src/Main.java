96a93b241fef8c0e01ef0e2861b46f329e2b9468
Slashaershov <127024236+Slashaershov@users.noreply.github.com>
2025-02-26 07:03:52 +0300

    add ex 1,2,3,4

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,72 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello, World!");
+        //ex1
+        byte clientOS = 0;
+        switch (clientOS)
+        {
+            case 0:
+                System.out.println("Установите версию приложения для iOS по ссылке");
+                break;
+            case 1:
+                System.out.println("Установите версию приложения для Android по ссылке");
+                break;
+        }
+        System.out.println();
+
+        //ex2
+        short yearIssue = 2010;
+        switch (clientOS)
+        {
+            case 0:
+                if (yearIssue >= 2015)
+                {
+                    System.out.println("Установите версию приложения для iOS по ссылке");
+                }
+                else
+                {
+                    System.out.println("Установите облегченную версию приложения для Android по ссылке");
+                }
+                break;
+            case 1:
+                System.out.println("Установите версию приложения для Android по ссылке");
+                break;
+        }
+        System.out.println();
+
+        //ex 3
+        short year = 2021;
+        if (year >= 1584 && year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+        {
+            System.out.println(year + " год является високосным");
+        }
+        else
+        {
+            System.out.println(year + " год не является високосным");
+        }
+
+        //ex 4
+        int deliveryDistance = 95;
+        int deliveryDaysCount = 0;
+        if (deliveryDistance <= 100)
+        {
+            if (deliveryDistance <= 20)
+            {
+                deliveryDaysCount = 1;
+            }
+            if (deliveryDistance <= 60)
+            {
+                deliveryDaysCount++;
+            }
+            if (deliveryDistance <= 100)
+            {
+                deliveryDaysCount++;
+            }
+            System.out.println("Потребуется дней: " + deliveryDaysCount);
+        }
+        System.out.println();
+
+        //ex 5
+
     }
 }
